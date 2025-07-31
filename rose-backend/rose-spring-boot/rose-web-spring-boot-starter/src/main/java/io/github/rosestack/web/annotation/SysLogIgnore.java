a089b8d16b3701a7172e054f8ac215c4d49337d9
陈志军 <zhijun.chen@dmall.com>
2025-07-31 14:57:47 +0800

    refactor: 修正审计日志系统

--- rose-backend/rose-spring-boot/rose-web-spring-boot-starter/src/main/java/io/github/rosestack/web/annotation/SysLogIgnore.java
+++
@@ -1,8 +0,0 @@
-package io.github.rosestack.web.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface SysLogIgnore {}
